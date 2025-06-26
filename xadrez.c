2e501c750374b74a85c4b9f50739b2d352414208
Moises Corbellari <moisescorbellari1@outlook.com>
2025-06-26 03:04:24 -0300

    refactor (1.Nível Novato): substituição de t, b e r por torre, bispo e rainha.

--- xadrez.c
+++ xadrez.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 int main(){
     // usando a estrutura for, while e do-while para mover as peças de xadrez (torre, bispo, rainha)
-    int t, b = 1, r = 1;
-    
+
     // torre
     printf("Movimento da Torre, 5 casas à direita:\n");
-    for (t = 1; t<= 5; t++){
-        printf("%dª casa à *direita*\n", t);
+    for (int torre = 1; 
+        torre<= 5; 
+        torre++){
+        printf("%dª casa à *direita*\n", torre);
     }
 
     printf("\n");
 
     // bispo
-    printf("Movimento do bispo, 5 casas na diagonal (Cima, Direita):\n");
-    while (b <= 5){
-        printf("%dª casa na *diagonal*\n", b);
-        b++;
+    printf("Movimento do bispo, 5 casas na diagonal (Cima, Direita):\n");{
+
+        int bispo = 1;
+        while (bispo <= 5){
+            printf("%dª casa na *diagonal*\n", bispo);
+            bispo++;
+        }
     }
 
     printf("\n");
 
     // rainha
-    printf("Movimento da rainha, 8 casas à esquerda:\n");
-    do{
-        printf("%dª casa à *esquerda*\n", r);
-        r++;
-    }while(r <= 8);
+    printf("Movimento da rainha, 8 casas à esquerda:\n");{
+
+        int rainha = 1;
+        do{
+            printf("%dª casa à *esquerda*\n", rainha);
+            rainha++;
+        }while(rainha <= 8);
+    }
     
     return 0;
 }
