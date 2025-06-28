d6fd000b5908183ad63c63fc14180800edaaac80
Moises Corbellari <moisescorbellari1@outlook.com>
2025-06-27 21:16:37 -0300

    2.Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     // usando a estrutura for, while e do-while para mover as peças de xadrez (torre, bispo, rainha)
 
     // torre
     printf("Movimento da Torre, 5 casas à direita:\n");
-    for (int torre = 1; 
-        torre<= 5; 
-        torre++){
+    for (int torre = 1;
+         torre <= 5;
+         torre++)
+    {
         printf("%dª casa à *direita*\n", torre);
     }
 
     printf("\n");
 
     // bispo
-    printf("Movimento do bispo, 5 casas na diagonal (Cima, Direita):\n");{
+    printf("Movimento do bispo, 5 casas na diagonal (Cima, Direita):\n");
+    {
 
         int bispo = 1;
-        while (bispo <= 5){
+        while (bispo <= 5)
+        {
             printf("%dª casa na *diagonal*\n", bispo);
             bispo++;
         }
@@ -25,14 +29,37 @@ int main(){
     printf("\n");
 
     // rainha
-    printf("Movimento da rainha, 8 casas à esquerda:\n");{
+    printf("Movimento da rainha, 8 casas à esquerda:\n");
+    {
 
         int rainha = 1;
-        do{
+        do
+        {
             printf("%dª casa à *esquerda*\n", rainha);
             rainha++;
-        }while(rainha <= 8);
+        } while (rainha <= 8);
     }
-    
+
+    printf("\n");
+
+    // cavalo
+    printf("Movimento do cavalo em 'L':\n");{
+        int movimento = 1;
+        int cavalo = 1;
+
+        while (movimento > 0)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                printf("%dª casa: cima\n", cavalo);
+                cavalo++;
+            }
+            printf("%dª casa: esquerda\n", cavalo);
+            cavalo++;
+
+            movimento--;
+        }
+    }
+
     return 0;
 }
